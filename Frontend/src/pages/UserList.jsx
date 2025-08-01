70ed7f8f814efc304053fbed87df56f447d86751
saidatemur <167565013+saidatemur@users.noreply.github.com>
2025-08-01 13:19:26 +0500

    UserList.jsx updated

--- Frontend/src/pages/UserList.jsx
+++ Frontend/src/pages/UserList.jsx
@@ -22,7 +22,7 @@ const UserList = () => {
             (a, b) => new Date(b.lastLogin) - new Date(a.lastLogin)
           );
           setUsers(sorted);
-          if (data.length === 0 || data.every((u) => u.isBlocked)) {
+          if (data.length === 0 || data.every((u) => u.isBlocked=== true || u.isBlocked === "true")) {
             navigate("/");
           }
         }
